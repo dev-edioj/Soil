0c25dfd915fe4269eb099be2cc23303acc9f811d
edio <dev.edioj@gmail.com>
2022-02-25 02:17:41 -0300

    inicio screen refeicao

--- front-end/src/screens/refeicao/index.js
+++ front-end/src/screens/refeicao/index.js
@@ -1,23 +1,80 @@
-import react from "react";
-import { Link } from "react-router-dom";
+import react, {useState, useEffect} from "react";
+import api from "../../services/api";
 
 import "../../style.css";
 
 
 const MostrarRefeicoes = () => {
+  const [refeicoes, setRefeicoes] = useState([])
+  useEffect(() => {
+    api.get('/refeicoes')
+      .then((response) => {
+        setRefeicoes(response.data); 
+      })
+      .catch(error => {
+        console.log("Deu errado")
+      })
+  }, [])
+  
  
-
   return (
+    
     <div className="container">
       <div className="container-table">
         <div className="table-title">
-          <h1 className="title">Minhas refeicoes</h1>
+          
         </div>
-        <div className="wrap-table"></div>
-        <div className="container-table--btn ">
+        <div className="wrap-table">
+          {refeicoes.map((refeicoes, key) => {
+            
+           
+            return (
+              <table className="dados_refeicoes" key={key}>
+                <caption>
+                {/*   <p>{JSON.stringify(refeicoes.itens)}</p> */}
+                  <h2 className="title">Minhas refeicoes</h2>
+                </caption>
+                <tr>
+                  <th>Usuario</th>
+                  <th>Tipo</th>
+                  <th>Categoria</th>
+                  <th>Descrição</th>
+                  <th>Quantidade</th>
+                  <th>Data</th>
+                </tr>
+                <tr>
+                  <td>{refeicoes.usuario_id}</td>
+                  <td>{refeicoes.tipo_refeicao}</td>
+                  <td>
+                    {refeicoes.itens.map((itens) => {
+                      return itens.tipo_item;
+                    })}
+                  </td>
+                  <td>
+                    {refeicoes.itens.map((itens) => {
+                      return itens.descricao;
+                    })}
+                  </td>
+                  <td>
+                    {refeicoes.itens.map((itens) => {
+                      return itens.quantidade;
+                    })}
+                  </td>
+                  <td>
+                    {refeicoes.itens.map((itens) => {
+                      return itens.created_at;
+                    })}
+                  </td>
+                </tr>
+              </table>
+            );
+           
+         })}
+        </div>
+        <div className="container-table-btn ">
           <input className="table-btn" type="submit" value="Adicionar" />
-          <input className="table-form-btn" type="submit" value="Editar" />
-          <input className="table-form-btn" type="submit" value="Deletar" />
+          <input className="table-btn" type="submit" value="Editar" />
+          <input className="table-btn" type="submit" value="Deletar" />
         </div>
       </div>
     </div>
